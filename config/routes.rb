274265e31ff8c875c659ab94e7606ca88d171eb7
Thomas Ngo <thomasngo818@gmail.com>
2022-03-16 21:07:13 +0000

    Update with routes for showing user login form, logging in and logging out

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,11 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   post 'users' => 'users#create'
 
+  #login routes
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
